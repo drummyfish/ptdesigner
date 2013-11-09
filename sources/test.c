88a6c66ed09ee41f8483c6c0dbe68279dbf3bdfb
Miloslav Číž <miloslav@localhost.localdomain>
2013-11-09 15:29:30 +0100

    example pictures

--- sources/test.c
+++ sources/test.c
@@ -10,68 +10,27 @@
 
 int main()
   {
-    double intensity;
-    int i;
+    t_color_buffer b,b2,b3,b4;
+    t_grammar g;
 
-    t_color_buffer buffer,buffer2,buffer3,buffer4;
+    color_buffer_init(&b,1024,1024);
 
-    color_buffer_init(&buffer,512,512);
-    color_buffer_init(&buffer2,512,512);
+    grammar_load_from_file(&g,"testgrammar.txt",4567);
 
-    pt_voronoi_diagram_simple(15478,20,&buffer2);
+    grammar_generate_string(&g,50);
 
-    pt_normal_map(&buffer2,15,&buffer3);
+    pt_turtle_draw(&b,&g,0.5,0.35,0.0,0.2,0.8);
 
-    intensity = 0.0;
+    pt_supersampling(&b,2,&b2);
 
-    for (i = 0; i < 5; i++)
-      {
-    pt_light(&buffer3,&buffer4,
-      5,3,0,
-      50,175,50,
-      200,255,200,
-      1.0,
-      REFLECTION_CURVE_COSINE_SMOOTH,
-      intensity,
-      0.7,0.7);
+    color_buffer_save_to_png(&b2,"pic.png");
 
-        intensity += 0.5;
-
-      switch (i)
-        {
-          case 0:
-            color_buffer_save_to_png(&buffer4,"pic.png");
-            break;
-
-          case 1:
-            color_buffer_save_to_png(&buffer4,"pic2.png");
-            break;
-
-          case 2:
-            color_buffer_save_to_png(&buffer4,"pic3.png");
-            break;
-
-          case 3:
-            color_buffer_save_to_png(&buffer4,"pic4.png");
-            break;
-
-          case 4:
-            color_buffer_save_to_png(&buffer4,"pic5.png");
-            break;
-        }
-
-      }
-
-
-
-    color_buffer_destroy(&buffer);
-    color_buffer_destroy(&buffer2);
-    color_buffer_destroy(&buffer3);
-    color_buffer_destroy(&buffer4);
+    color_buffer_destroy(&b);
+    color_buffer_destroy(&b2);
 
     return 0;
 
-    /*
+      /*
     t_square_mosaic mosaic;
     t_color_buffer buffer;
 
